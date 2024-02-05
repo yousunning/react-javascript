fccef5ba08e0c1dacd49f2b7a1f33356613011ce
yousunning <151820436+yousunning@users.noreply.github.com>
2024-02-05 17:25:41 +0900

    Create RepoMenu.js

---
+++ Fetch-Hooks/src/RepoMenu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useIterator } from "./hooks";
+import RepositoryReadme from "./RepositiryReadme";
+export default function RepoMenu({ repositories, login }) {
+  const [{ name }, previous, next] = useIterator(repositories);
+
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <button onClick={previous}>&lt;</button>
+        <p>{name}</p>
+        <button onClick={next}>&gt;</button>
+      </div>
+      <RepositoryReadme login={login} repo={name} />
+    </>
+  );
+}
