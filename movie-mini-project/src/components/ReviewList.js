6b875a17d6328e7e5e10f8eac5b986039d755508
yousunning <151820436+yousunning@users.noreply.github.com>
2024-02-06 16:57:32 +0900

    Create ReviewList.js

---
+++ movie-mini-project/src/components/ReviewList.js
@@ -0,0 +1,46 @@
+import "./ReviewList.css";
+import StarRating from "./StarRating";
+
+function formatData(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+}
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => {
+    onDelete(item.id);
+  };
+  return (
+    <div className="ReviewListItem">
+      <img
+        className="ReviewListItem-img"
+        src="{item.imgUrl}"
+        alt="{item.title}"
+      />
+      <div>
+        <h3>{item.title}</h3>
+        <p>
+          <StarRating selectedStars={item.rating} />
+        </p>
+        <p>{formatData(item.createdAt)}</p>
+        <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
+      </div>
+    </div>
+  );
+}
+function ReviewList({ items, onDelete }) {
+  //console.log(items);
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            <ReviewListItem item={item} onDelete={onDelete} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default ReviewList;
