1b281492843e9497fc8539b216dc0fe2becf7911
yousunning <151820436+yousunning@users.noreply.github.com>
2024-02-06 15:10:11 +0900

    Create sum.js

---
+++ react-jest/src/sum.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
+
+
+//https://jestjs.io/docs/getting-started
+//npm install --save-dev jest
+//package.json 파일에 밑 제이슨형식의 테스트 추가하기
+// {
+//   "scripts": {
+//     "test": "jest"
+//   }
+// }
